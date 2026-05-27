f76ddd9985e4e496514993349481b5817d12c3fd
Husen <kasabhusen5@gmail.com>
2026-05-28 00:32:21 +0530

    Clean Todo App

---
+++ fastapi_app.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import sqlite3
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+conn = sqlite3.connect("todos.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT
+)
+""")
+
+conn.commit()
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+
+    cursor.execute("SELECT * FROM todos")
+    todos = cursor.fetchall()
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "todos": todos
+        }
+    )
+
+@app.post("/add")
+async def add(task: str = Form(...)):
+
+    cursor.execute(
+        "INSERT INTO todos (task) VALUES (?)",
+        (task,)
+    )
+
+    conn.commit()
+
+    return RedirectResponse("/", status_code=303)
+
+@app.get("/delete/{todo_id}")
+async def delete(todo_id: int):
+
+    cursor.execute(
+        "DELETE FROM todos WHERE id = ?",
+        (todo_id,)
+    )
+
+    conn.commit()
+
+    return RedirectResponse("/", status_code=303)

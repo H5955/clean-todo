4badc971d6593a9de1e30403c7cd81389603545e
Husen <kasabhusen5@gmail.com>
2026-05-31 13:52:41 +0530

    fix postgres connections

--- api/index.py
+++ api/index.py
@@ -7,14 +7,17 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg.connect(DATABASE_URL)
-cursor = conn.cursor()
+def get_conn():
+    return psycopg.connect(DATABASE_URL)
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
+
+# Create table
+conn = get_conn()
+cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS todos (
@@ -25,12 +28,20 @@ CREATE TABLE IF NOT EXISTS todos (
 """)
 
 conn.commit()
+cursor.close()
+conn.close()
 
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    conn = get_conn()
+    cursor = conn.cursor()
+
     cursor.execute("SELECT * FROM todos ORDER BY id DESC")
     todos = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
 
     completed_count = sum(todo[2] for todo in todos)
 
@@ -47,18 +58,26 @@ async def home(request: Request):
 
 @app.post("/add")
 async def add(task: str = Form(...)):
+    conn = get_conn()
+    cursor = conn.cursor()
+
     cursor.execute(
         "INSERT INTO todos (task, completed) VALUES (%s, %s)",
         (task, 0)
     )
 
     conn.commit()
+    cursor.close()
+    conn.close()
 
     return RedirectResponse("/", status_code=303)
 
 
 @app.get("/toggle/{todo_id}")
 async def toggle(todo_id: int):
+    conn = get_conn()
+    cursor = conn.cursor()
+
     cursor.execute(
         "SELECT completed FROM todos WHERE id=%s",
         (todo_id,)
@@ -76,16 +95,25 @@ async def toggle(todo_id: int):
 
         conn.commit()
 
+    cursor.close()
+    conn.close()
+
     return RedirectResponse("/", status_code=303)
 
 
 @app.get("/delete/{todo_id}")
 async def delete(todo_id: int):
+    conn = get_conn()
+    cursor = conn.cursor()
+
     cursor.execute(
         "DELETE FROM todos WHERE id=%s",
         (todo_id,)
     )
 
     conn.commit()
+
+    cursor.close()
+    conn.close()
 
     return RedirectResponse("/", status_code=303)
